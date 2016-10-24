dd8c02a32c1b425351b13e14674c441b9994b562
Micah <micah@dvyld.com>
2016-10-24 11:12:49 -0700

    Updating setup file to install bin correctly.

--- bin/setupdotfiles.sh
+++ bin/setupdotfiles.sh
@@ -4,4 +4,4 @@ ln $DIR/../dotfiles/.tmux.conf ~/.tmux.conf
 ln $DIR/../dotfiles/.zshrc ~/.zshrc
 ln $DIR/../dotfiles/.vimrc ~/.vimrc
 cp -al $DIR/../dotfiles/.vim ~/.vim
-cp -al $DIR ~/
+cp -al $DIR ~/bin
