773300fd741e1cef928daae2f7b7841f5eca069e
Micah <micah@dvyld.com>
2016-12-08 22:41:54 -0600

    Removing unneeded cp/link command.

--- bin/setupdotfiles.sh
+++ bin/setupdotfiles.sh
@@ -4,5 +4,4 @@ ln $DIR/../dotfiles/.tmux.conf ~/.tmux.conf
 ln $DIR/../dotfiles/.zshrc ~/.zshrc
 ln $DIR/../dotfiles/.vimrc ~/.vimrc
 cp -al $DIR/../dotfiles/.vim ~/.vim
-cp -al $DIR/../dotfiles/.vim/ftplugin ~/.vim/ftplugin
 cp -al $DIR ~/bin
